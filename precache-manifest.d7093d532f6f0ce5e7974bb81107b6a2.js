d973a88860646a0d5bec1712c0fa298ad50fc71a
shahman28 <42969025+shahman28@users.noreply.github.com>
2020-10-07 18:09:03 -0400

    Updates

---
+++ precache-manifest.d7093d532f6f0ce5e7974bb81107b6a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1a32c89fd2a87618c815163c2a5d9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea1bab0d4af4ad87df6",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "9d1b7c3324fc2526a5f2",
+    "url": "/static/css/main.f404daad.chunk.css"
+  },
+  {
+    "revision": "5ea1bab0d4af4ad87df6",
+    "url": "/static/js/2.292beaba.chunk.js"
+  },
+  {
+    "revision": "7a9ffbcb0705b685a94df36082c8d5cd",
+    "url": "/static/js/2.292beaba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1b7c3324fc2526a5f2",
+    "url": "/static/js/main.c39d548c.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  },
+  {
+    "revision": "93cd60c76599a8479a9402babe302db4",
+    "url": "/static/media/Page One 2019_Color_-13(1).93cd60c7.jpg"
+  }
+]);
